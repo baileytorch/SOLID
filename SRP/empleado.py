f8240d1268878cc37cdd6aa0782a95ee55447050
Erick Bailey <baileytorch@hotmail.com>
2024-09-24 17:45:59 -0300

    Iniciando Repo en Laptop

--- SRP/empleado.py
+++ SRP/empleado.py
@@ -8,7 +8,9 @@ class Empleado():
     def reponer_stock():
         pass
 
-""" En un pequeño negocio esto no parece extraño porque los empleados tienden a realizar varias tareas,
-pero que sucedería si con el tiempo el negocio crece y nos piden funcionalidades adicionales como pagar con diferentes métodos de pago, 
-por ejemplo tarjeta de crédito y débito, 
-además el negocio contrata gente adicional para que trabajen exclusivamente en la reposición del stock. """
+"""
+En un pequeño negocio esto no parece extraño porque los empleados tienden a realizar varias tareas,
+pero ¿que sucedería si con el tiempo el negocio crece y nos piden funcionalidades adicionales como pagar con diferentes métodos de pago?, 
+por ejemplo tarjeta de crédito y débito. 
+Por otro lado, el negocio podría contratar gente adicional para que trabajen exclusivamente en la reposición del stock. 
+"""
